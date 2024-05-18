cc7759e25109b8c642bfe45f544935334023ec30
SMNoushan <noushan.md@gmail.com>
2024-05-18 15:51:12 +0600

    added footer

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/shared/Footer";
 
 const Root = () => {
   return (
     <>
       <h1 className="text-3xl font-inter">This is Root</h1>
       <Outlet />
+      <Footer />
     </>
   );
 };
