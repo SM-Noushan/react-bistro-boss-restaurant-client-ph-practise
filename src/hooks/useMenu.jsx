84190ec1537cc5bde1d7ed721280cca394a3161f
SMNoushan <noushan.md@gmail.com>
2024-05-21 03:54:41 +0600

    added useMenu hook

---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+const useMenu = (qKey, type = null) => {
+  const { data, isLoading, refetch, error } = useQuery({
+    queryKey: [qKey],
+    queryFn: async () => {
+      try {
+        const res = await axios.get("data/menu.json");
+        if (type)
+          return await res.data.filter(
+            (item) => item.category.toLowerCase() === type
+          );
+        return await res.data;
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
+  });
+  return { data, isLoading, refetch, error };
+};
+
+useMenu.propTypes = {
+  qKey: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
+
+export default useMenu;
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,25 +1,20 @@
 import TitleBanner from "../../components/shared/title-banner/TitleBanner";
 import MenuItem from "../../components/menu-item/MenuItem";
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
 import axios from "axios";
 import SectionHeading from "../../components/home/SectionHeading";
 import bannerURL from "../../assets/menu/banner3.jpg";
 import bannerURL2 from "../../assets/home/chef-service.jpg";
+import useMenu from "../../hooks/useMenu";
 
 const Menu = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["allMenu"],
-    queryFn: async () => {
-      try {
-        const res = await axios.get("data/menu.json");
-        return await res.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
+  
   return (
     <>
+      <Helmet>
+        <title>BistroBoss | Our Menu</title>
+      </Helmet>
       <TitleBanner
         imgURL={bannerURL}
         heading={"OUR MENU"}
