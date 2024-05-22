296beee7e6bd3cb4923bd84948abd2c14c546fac
SMNoushan <noushan.md@gmail.com>
2024-05-22 11:57:23 +0600

    added scroll to top func

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/shared/Footer";
 import Header from "../components/shared/Header";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Root = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
