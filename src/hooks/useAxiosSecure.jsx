5fdac4d8853fe4bfa0a8a36e3533665f896ea738
SMNoushan <noushan.md@gmail.com>
2024-05-24 05:12:35 +0600

    token verification

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,39 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000/",
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `bearer ${token}`;
+      return config;
+    });
+    return () => axios.interceptors.request.eject(reqInterceptor);
+  }, []);
+
+  useEffect(() => {
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (res) => res,
+      async (error) => {
+        const status = error.response.status;
+        if (status === 401 || status === 402) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+    return () => axios.interceptors.request.eject(resInterceptor);
+  }, []);
   return axiosSecure;
 };
 
