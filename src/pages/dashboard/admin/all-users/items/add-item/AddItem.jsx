64c735030b2fb783b7b6ec323ddabad65d2bb279
SMNoushan <noushan.md@gmail.com>
2024-05-24 20:40:37 +0600

    added add item to db

--- src/pages/dashboard/admin/all-users/items/add-item/AddItem.jsx
+++ src/pages/dashboard/admin/all-users/items/add-item/AddItem.jsx
@@ -1,12 +1,78 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa6";
+import useAxiosSecure from "../../../../../../hooks/useAxiosSecure";
 import SectionHeading from "../../../../../../components/home/SectionHeading";
 
+// Error message for form fields
+const errorMsg = (message) => (
+  <div className=" mt-2 flex items-center gap-1 text-red-400 text-xs">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="-mt-px h-4 w-4 "
+    >
+      <path
+        fillRule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clipRule="evenodd"
+      />
+    </svg>
+    <p>{message}</p>
+  </div>
+);
+
+// Function to generate a unique file name
+const generateUniqueFileName = (originalFileName) => {
+  const timestamp = Date.now();
+  const randomString = Math.random().toString(36).substring(7);
+  return `${timestamp}_${randomString}_${originalFileName}`;
+};
+
 const AddItem = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm();
+  const axiosSecure = useAxiosSecure();
+
+  const handleForm = async (data) => {
+    try {
+      const imageFile = new FormData();
+      const originalFileName = data.image[0].name;
+      const uniqueFileName = generateUniqueFileName(originalFileName);
+      console.log(uniqueFileName, originalFileName);
+      imageFile.append("image", data.image[0], uniqueFileName);
+      console.log(imageFile);
+      const res = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API}`,
+        imageFile
+      );
+      if (res.data.success) {
+        data.image = res.data.data.display_url;
+        data.price = parseFloat(data.price);
+        const resDB = await axiosSecure.post("/menu", data);
+        console.log(resDB);
+        if (resDB.data.insertedId) {
+          reset();
+          toast.success("Recipe added");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed! Please try again");
+    }
+  };
   return (
     <>
       <SectionHeading heading="ADD AN ITEM" subHeading="What's new?" />
       <div className="p-12 bg-dark-007">
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit(handleForm)} className="space-y-4">
           {/* recipe name */}
           <div className="form-control">
             <label className="label px-0">
@@ -16,12 +82,13 @@ const AddItem = () => {
             </label>
             <input
               autoComplete="recipe-name"
-              name="recipeName"
+              name="recipe-name"
               type="text"
               placeholder="Recipe name"
               className="input input-bordered placeholder:text-dark-1a1"
-              required
+              {...register("name", { required: true })}
             />
+            {errors?.name && errorMsg("Recipe Name Required")}
           </div>
           {/* price and category */}
           <div className="flex gap-6 *:flex-1">
@@ -35,13 +102,26 @@ const AddItem = () => {
               <select
                 name="category"
                 className="select select-bordered text-dark-1a1 text-base"
+                defaultValue={"category"}
+                {...register("category", {
+                  required: true,
+                  pattern: {
+                    value: /^(?!category$).*$/i,
+                  },
+                })}
               >
-                <option disabled selected>
+                <option disabled value="category">
                   Category
                 </option>
-                <option>Han Solo</option>
-                <option>Greedo</option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soups">Soups</option>
+                <option value="desserts">Deserts</option>
+                <option value="drinks">Drinks</option>
+                <option value="popular">Popular</option>
+                <option value="offered">Offered</option>
               </select>
+              {errors?.category && errorMsg("Select a category")}
             </div>
             {/* recipe price */}
             <div className="form-control">
@@ -55,9 +135,14 @@ const AddItem = () => {
                 name="price"
                 type="number"
                 placeholder="Price"
+                min={0}
                 className="input input-bordered placeholder:text-dark-1a1"
-                required
+                {...register("price", {
+                  required: true,
+                  min: 0,
+                })}
               />
+              {errors?.price && errorMsg("Required recipe price")}
             </div>
           </div>
           {/* recipe details */}
@@ -74,8 +159,11 @@ const AddItem = () => {
               placeholder="Recipe details"
               className="textarea textarea-bordered placeholder:text-dark-1a1"
               rows={8}
-              required
+              {...register("recipe", {
+                required: true,
+              })}
             />
+            {errors?.recipe && errorMsg("Recipe Details Required")}
           </div>
 
           {/* recipe image */}
@@ -83,7 +171,9 @@ const AddItem = () => {
             name="recipeImage"
             type="file"
             className="file-input file:bg-dark-006 file:border-none file:text-dark-002 w-full max-w-xs bg-dark-007"
+            {...register("image", { required: true })}
           />
+          {errors?.image && errorMsg("Upload recipe image")}
 
           {/* form submit button */}
           <div className="form-control mt-6 w-fit">
